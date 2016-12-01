b0471653042e681015b395c5b7c61c3dff455886
AlvaroLarumbe <biobiotm@gmail.com>
2016-12-01 20:52:39 +0100

    Day 1 - Part 1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Advent of Code 2016 Solutions
-| Day | Title                                                       | Status    |
-| --- | ----------------------------------------------------------- |:---------:|
-| 1   | [No Time for a Taxicab](http://adventofcode.com/2016/day/1) | :warning: |
+| Day | Title                                                       | Status                |
+| --- | ----------------------------------------------------------- |:---------------------:|
+| 1   | [No Time for a Taxicab](http://adventofcode.com/2016/day/1) | :warning: Only Part 1 |
---
+++ src/taxicab.distance.js
@@ -0,0 +1,77 @@
+/**
+ * Day 1: No Time for a Taxicab
+ * 
+ * Usage: node taxicab.distance.js ../data/input.txt
+ */
+
+var Promise = require("bluebird");
+var fs = Promise.promisifyAll(require("fs"));
+
+'use strict';
+
+var src = [0, 0];
+var dst = [0, 0];
+var lastDirection = 'N'; // North, East, South, West
+var movements = [];
+
+var input = fs.readFileAsync(process.argv[2], { encoding: 'utf8', flag: 'r' });
+
+input
+	.then(function(data) {
+		var movements = data.split(', ');
+
+		movements.forEach(function(val) {
+			move(val[0], val.substr(1));
+		});
+
+		console.log('Source: [' + src[0] + ', ' + src[1] + ']');
+		console.log('Destination: [' + dst[0] + ', ' + dst[1] + ']');
+		console.log('Taxicab Distance: ' + (Math.abs(src[0] - dst[0]) + Math.abs(src[1] - dst[1])));
+	})
+	.catch(function(data) {
+		console.log(data.cause);
+	});
+
+function move(direction, steps) {
+	steps = parseInt(steps);
+
+	if (direction === 'R') {
+		switch (lastDirection) {
+			case 'N':
+				dst[0] += steps;
+				lastDirection = 'E';
+				break;
+			case 'E':
+				dst[1] -= steps;
+				lastDirection = 'S';
+				break;
+			case 'S':
+				dst[0] -= steps;
+				lastDirection = 'W';
+				break;
+			case 'W':
+				dst[1] += steps;
+				lastDirection = 'N';
+				break;
+		}
+	} else if (direction === 'L') {
+		switch (lastDirection) {
+			case 'N':
+				dst[0] -= steps;
+				lastDirection = 'W';
+				break;
+			case 'E':
+				dst[1] += steps;
+				lastDirection = 'N';
+				break;
+			case 'S':
+				dst[0] += steps;
+				lastDirection = 'E';
+				break;
+			case 'W':
+				dst[1] -= steps;
+				lastDirection = 'S';
+				break;
+		}
+	}
+}
