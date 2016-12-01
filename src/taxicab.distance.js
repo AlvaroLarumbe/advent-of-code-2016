9acb3247b8817f76f9af7856b399f8d5268005e8
AlvaroLarumbe <biobiotm@gmail.com>
2016-12-01 20:57:42 +0100

    Fix package name and convert to spaces

--- src/taxicab.distance.js
+++ src/taxicab.distance.js
@@ -17,61 +17,61 @@ var movements = [];
 var input = fs.readFileAsync(process.argv[2], { encoding: 'utf8', flag: 'r' });
 
 input
-	.then(function(data) {
-		var movements = data.split(', ');
+    .then(function(data) {
+        var movements = data.split(', ');
 
-		movements.forEach(function(val) {
-			move(val[0], val.substr(1));
-		});
+        movements.forEach(function(val) {
+            move(val[0], val.substr(1));
+        });
 
-		console.log('Source: [' + src[0] + ', ' + src[1] + ']');
-		console.log('Destination: [' + dst[0] + ', ' + dst[1] + ']');
-		console.log('Taxicab Distance: ' + (Math.abs(src[0] - dst[0]) + Math.abs(src[1] - dst[1])));
-	})
-	.catch(function(data) {
-		console.log(data.cause);
-	});
+        console.log('Source: [' + src[0] + ', ' + src[1] + ']');
+        console.log('Destination: [' + dst[0] + ', ' + dst[1] + ']');
+        console.log('Taxicab Distance: ' + (Math.abs(src[0] - dst[0]) + Math.abs(src[1] - dst[1])));
+    })
+    .catch(function(data) {
+        console.log(data.cause);
+    });
 
 function move(direction, steps) {
-	steps = parseInt(steps);
+    steps = parseInt(steps);
 
-	if (direction === 'R') {
-		switch (lastDirection) {
-			case 'N':
-				dst[0] += steps;
-				lastDirection = 'E';
-				break;
-			case 'E':
-				dst[1] -= steps;
-				lastDirection = 'S';
-				break;
-			case 'S':
-				dst[0] -= steps;
-				lastDirection = 'W';
-				break;
-			case 'W':
-				dst[1] += steps;
-				lastDirection = 'N';
-				break;
-		}
-	} else if (direction === 'L') {
-		switch (lastDirection) {
-			case 'N':
-				dst[0] -= steps;
-				lastDirection = 'W';
-				break;
-			case 'E':
-				dst[1] += steps;
-				lastDirection = 'N';
-				break;
-			case 'S':
-				dst[0] += steps;
-				lastDirection = 'E';
-				break;
-			case 'W':
-				dst[1] -= steps;
-				lastDirection = 'S';
-				break;
-		}
-	}
+    if (direction === 'R') {
+        switch (lastDirection) {
+            case 'N':
+                dst[0] += steps;
+                lastDirection = 'E';
+                break;
+            case 'E':
+                dst[1] -= steps;
+                lastDirection = 'S';
+                break;
+            case 'S':
+                dst[0] -= steps;
+                lastDirection = 'W';
+                break;
+            case 'W':
+                dst[1] += steps;
+                lastDirection = 'N';
+                break;
+        }
+    } else if (direction === 'L') {
+        switch (lastDirection) {
+            case 'N':
+                dst[0] -= steps;
+                lastDirection = 'W';
+                break;
+            case 'E':
+                dst[1] += steps;
+                lastDirection = 'N';
+                break;
+            case 'S':
+                dst[0] += steps;
+                lastDirection = 'E';
+                break;
+            case 'W':
+                dst[1] -= steps;
+                lastDirection = 'S';
+                break;
+        }
+    }
 }
